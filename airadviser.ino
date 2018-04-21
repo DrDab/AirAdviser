4a6e834bf031c12ac87ec57df5275959674de2c4
_c0da_ <duvictor514@gmail.com>
2018-04-21 12:07:07 -0700

    Add more to GET request handler

--- airadviser.ino
+++ airadviser.ino
@@ -277,6 +277,22 @@ void readTemps()
     }
 }
 
+String getValue(String data, char separator, int index)
+{
+    int found = 0;
+    int strIndex[] = { 0, -1 };
+    int maxIndex = data.length() - 1;
+
+    for (int i = 0; i <= maxIndex && found <= index; i++) {
+        if (data.charAt(i) == separator || i == maxIndex) {
+            found++;
+            strIndex[0] = strIndex[1] + 1;
+            strIndex[1] = (i == maxIndex) ? i+1 : i;
+        }
+    }
+    return found > index ? data.substring(strIndex[0], strIndex[1]) : "";
+}
+
 void loop() 
 {
   readTemps();
@@ -1026,7 +1042,17 @@ void runServer()
   else if (val == -9)
   {
     // parse the management page information.
-    
+    String eq = req;
+    String yiff = getValue(eq, '&', 0);
+    String part1 = getValue(yiff, '?', 0);
+    String part2 = getValue(eq, '&', 1);
+    String part3 = getValue(eq, '&', 2);
+    String part4 = getValue(eq, '&', 3);
+    Serial.println("MGMT REQUEST");
+    Serial.println(part1);
+    Serial.println(part2);
+    Serial.println(part3);
+    Serial.println(part4);
   }
   else
   {
